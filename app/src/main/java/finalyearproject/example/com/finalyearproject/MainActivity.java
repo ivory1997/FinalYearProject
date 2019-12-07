64fc554159970db7a397e4578e6057f9394a84c6
ivory1997 <mkivory97@gmail.com>
2019-12-07 17:26:29 +0000

    commit 4

--- app/src/main/java/finalyearproject/example/com/finalyearproject/MainActivity.java
+++ app/src/main/java/finalyearproject/example/com/finalyearproject/MainActivity.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
