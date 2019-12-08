e7893ae2411edf2e74f640a21cf84a48dce21908
ivory1997 <36885349+ivory1997@users.noreply.github.com>
2019-12-08 16:05:09 +0000

    Update server_connection.php

--- PHP files/server_connection.php
+++ PHP files/server_connection.php
@@ -3,7 +3,8 @@
 $servername = "localhost";
 $username = "id11003303_c16307271";
 $dbname = "id11003303_finalyearproject2";
-$password = "jesuschrist5624yes";
+//password not revealed in this repository
+$password = "";
 //Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 //Check connection
