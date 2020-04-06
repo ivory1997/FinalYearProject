28fc08159bce6c007463b1ffd35814e3e481ef41
ivory1997 <36885349+ivory1997@users.noreply.github.com>
2020-04-06 14:34:06 +0100

    Update edit.php

--- PHP files/edit.php
+++ PHP files/edit.php
@@ -6,13 +6,19 @@ $user_email = $_POST["identifier_updateEmail"];
 $user_pass = $_POST["identifier_updatePassword"];
 $user_name = $_POST["identifier_updateName"];
 
+$mysql_query2 = "SELECT * FROM users WHERE email = '$user_email'";
+$mysql_query3 = "SELECT * FROM users WHERE name = '$user_name'";
+$result2 = mysqli_query($conn,$mysql_query2);
+$result3 = mysqli_query($conn,$mysql_query3);
+
 $query = "UPDATE users 
 SET  email = '$user_email', password = '$user_pass', name = '$user_name'
 WHERE email = '$old_email'";
 $query2 = "UPDATE countries
 SET  email = '$user_email'
 WHERE email = '$old_email'";
-
+if(mysqli_num_rows($result2) = 0 && mysqli_num_rows($result3) = 0)
+{
 if(mysqli_query($conn,$query)){
 	echo "<h2>Data Successfully Updated!</h2>";
 }
@@ -24,5 +30,6 @@ if(mysqli_query($conn,$query2)){
 }
 else{
 	echo "<h2>Data was unable to be updated.</h2>";
+}
 }
 ?>
